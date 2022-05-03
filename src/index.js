d5a792b92a1e742abe09b55f20ed6d88f45f7d64
Tanya <tanaybrown@gmail.com>
2022-05-03 12:36:10 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 const init = () => {
+    const inputForm = document.querySelector('form')
   
-}
-
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+  
+      fetch(`http://localhost:3000/movies/${input.value}`)
+      .then(response => response.json())
+      .then(data => { 
+        let title = document.querySelector('section#movieDetails h4');
+        let summary = document.querySelector('section#movieDetails p')
+       if(data.title === undefined) {
+         title.innerText = "Sorry, data not found"
+         summary.innerText = "Sorry, data not found"
+        }
+        else {
+          title.innerText = data.title;
+          summary.innerText = data.summary;
+          }
+         console.log( typeof data.title)
+      })
+    });
+  }
 document.addEventListener('DOMContentLoaded', init);
